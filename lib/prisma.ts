75079e610963323b4879f9321c0fb27e01ee1eae
Rocco A <43506958+GenerelSchwerz@users.noreply.github.com>
2025-12-08 21:57:10 -0500

    yes

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,7 +1,6 @@
 // lib/prisma.ts
 import { PrismaClient } from "@/lib/generated/prisma/client";
 import { PrismaPg } from '@prisma/adapter-pg'
-import { withAccelerate } from "@prisma/extension-accelerate"
 
 const connectionString = `${process.env.DATABASE_URL}`
 
